9b0f8767f178cbb67b8436119ce7a8b9c32bbd4e
Vladimir Kalmykov <v.kalmykov@infotech.team>
2017-12-29 08:03:58 +0300

    support module.exports

--- src/index.js
+++ src/index.js
@@ -2,16 +2,17 @@ import isPromise from 'is-promise';
 import functionize from './helpers/functionize';
 
 function reflow(stage1, props) {
-  const stage2 = stage1.reduce(function(chain, promise) {
-    return chain.then(promise);
-  }, Promise.resolve(props));
+  const stage2 = stage1.reduce((chain, promise) => chain.then(promise), Promise.resolve(props));
   return stage2;
 }
 
-export default function flow(...units) {
+function flow(...units) {
   const stage1 = units
   .map(functionize)
   return function(props = {}) {
     return reflow(stage1, props);
   }
 }
+
+
+export default flow;
